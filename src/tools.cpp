54612dc5f3067ad77f69fbfc2f1afe463e4971da
RuslanLazarev <ruslan.lazarev@gmail.com>
2018-08-19 14:55:44 +0200

    update

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,42 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	if(estimations.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The estimations vector is empty" << endl;
+    return rmse;
+  }
+
+  if(ground_truth.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The ground-truth vector is empty" << endl;
+    return rmse;
+  }
+
+	//  * the estimation vector size should equal ground truth vector size
+	if(estimations.size() != ground_truth.size() || estimations.size() == 0) {
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+  //accumulate squared residuals
+	for(unsigned int i=0; i < estimations.size(); ++i) {
+		VectorXd residual = estimations[i] - ground_truth[i];
+
+		//coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse/estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
--- src/ukf.h
+++ src/ukf.h
@@ -67,6 +67,18 @@ public:
   ///* Sigma point spreading parameter
   double lambda_;
 
+  ///* Radar measurement noise covariance matrix
+  MatrixXd R_radar_;
+
+  ///* Lidar measurement noise covariance matrix
+  MatrixXd R_lidar_;
+
+  ///* the current NIS for radar
+  double NIS_radar_;
+
+  ///* the current NIS for laser
+  double NIS_laser_;
+
 
   /**
    * Constructor
